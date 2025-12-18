3bef17542c408c046a19b7eb0a6e75a7ae6cb1ca
Sachu-Meghana <sachumeghana@gmail.com>
2025-12-18 20:54:04 +0530

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -65,4 +65,5 @@ def get_top_n_similar_chunk_pair_indices(similarity, n):
             pairs.append((i, j, similarity[i][j]))
 
     pairs = sorted(pairs, key=lambda x: x[2], reverse=True)
-    return [(str(p[0]), str(p[1])) for p in pairs[:n]]
+    return [(i, j) for i, j, _ in pairs[:n]]
+
